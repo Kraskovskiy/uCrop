d88df761cab147860d6288bb7e6ffecd219465fe
igor-alyoshin <stawr88@bk.ru>
2017-06-16 15:09:12 +0300

    #0 [fix] не отправлялись файлы

--- ucrop/src/main/java/com/yalantis/ucrop/task/BitmapCropTask.java
+++ ucrop/src/main/java/com/yalantis/ucrop/task/BitmapCropTask.java
@@ -159,10 +159,10 @@ public class BitmapCropTask extends AsyncTask<Void, Void, Throwable> {
                     ExifInterface.TAG_ORIENTATION,
                     ExifInterface.ORIENTATION_NORMAL);
             if (orientation != 0) {
+                saveImage(Bitmap.createBitmap(mViewBitmap, left, top, mCroppedImageWidth, mCroppedImageHeight));
                 if (mCompressFormat.equals(Bitmap.CompressFormat.JPEG)) {
                     ImageHeaderParser.copyExif(originalExif, mViewBitmap.getWidth(), mViewBitmap.getHeight(), mImageOutputPath);
                 }
-                saveImage(Bitmap.createBitmap(mViewBitmap, left, top, mCroppedImageWidth, mCroppedImageHeight));
             } else {
                 FileUtils.copyFile(mImageInputPath, mImageOutputPath);
             }
